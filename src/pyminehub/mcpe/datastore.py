6825f1432c55d1f9ef0816d47e527d7fb32254da
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-08-16 11:55:03 +0300

    Update datastore.py

--- src/pyminehub/mcpe/datastore.py
+++ src/pyminehub/mcpe/datastore.py
@@ -42,33 +42,39 @@ class DataStore:
 class _DataBase(DataStore):
 
     def __init__(self, name: str) -> None:
-        exists = os.path.isfile(name + '.json')
-        self._file = open(name + '.json', '+w')
-        if exists:
+        self._path = name + '.json'
+        if os.path.isfile(self._path):
             try:
-                self._data = json.load(self._file)
+                with open(self._path, 'r') as file:
+                    self._data = json.load(file)
+                    file.close()
             except Exception:
                 self._data = {}
         else:
             self._data = {}
         self._create_table()
+        
+    def _save_table(self) -> None:
+        with open(self._path, 'w') as file:
+            json.dump(self._data, file)
+            file.close()
 
     def _create_table(self) -> None:
         if "chunk" not in self._data:
             self._data["chunk"] = {}
         if "player" not in self._data:
             self._data["player"] = {}
-        json.dump(self._data, self._file)
+        self._save_table()
 
     def delete_all(self) -> None:
         del self._data["chunk"]
         del self._data["player"]
-        json.dump(self._data, self._file)
+        self._save_table()
 
     def save_chunk(self, position: ChunkPosition, chunk: Chunk, insert_only=False) -> None:
         encoded_chunk = encode_chunk(chunk)
         self._data["chunk"][":".join([str(position.x), str(position.z)])] = binascii.hexlify(encoded_chunk).decode()
-        json.dump(self._data, self._file)
+        self._save_table()
 
     def load_chunk(self, position: ChunkPosition) -> Optional[Chunk]:
         param = ":".join([str(position.x), str(position.z)])
@@ -80,7 +86,7 @@ class _DataBase(DataStore):
 
     def save_player(self, player_id: str, player: PlayerState, insert_only=False) -> None:
         self._data["player"][player_id] = binascii.hexlify(pickle.dumps(player, protocol=_PICKLE_PROTOCOL)).decode()
-        json.dump(self._data, self._file)
+        self._save_table()
 
     def load_player(self, player_id: str) -> Optional[PlayerState]:
         if player_id in self._data["player"]:
